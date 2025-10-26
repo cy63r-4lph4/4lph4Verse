4b52acb1e8fe21a727e9e0f67f96b207a7857320
cy63r-4lph4 <cy63r.4lph4@gmail.com>
2025-10-26 18:56:52 +0000

    chore: converted the permittypeddata into a permittypeddata builder to support dynamic chains

--- apps/relayer/src/utils/permitTypedData.ts
+++ apps/relayer/src/utils/permitTypedData.ts
@@ -1,21 +1,23 @@
-import { ChainId, deployedContracts } from "@verse/sdk";
+import { ChainId, getDeployedContract } from "@verse/sdk";
 
-const chainId = Number(process.env.CHAIN_ID || 11142220) as ChainId;
+export function buildPermitTypedData(chainId: ChainId) {
+  const CoreToken = getDeployedContract(chainId, "CoreToken");
 
-export const permitTypedData = {
-  domain: {
-    name: "Alph4 Core",
-    version: "1",
-    chainId,
-    verifyingContract: deployedContracts[chainId].CoreToken.address
-  },
-  types: {
-    Permit: [
-      { name: "owner", type: "address" },
-      { name: "spender", type: "address" },
-      { name: "value", type: "uint256" },
-      { name: "nonce", type: "uint256" },
-      { name: "deadline", type: "uint256" }
-    ]
-  }
-};
+  return {
+    domain: {
+      name: "Alph4 Core",
+      version: "1",
+      chainId,
+      verifyingContract: CoreToken.address,
+    },
+    types: {
+      Permit: [
+        { name: "owner", type: "address" },
+        { name: "spender", type: "address" },
+        { name: "value", type: "uint256" },
+        { name: "nonce", type: "uint256" },
+        { name: "deadline", type: "uint256" },
+      ],
+    },
+  };
+}
