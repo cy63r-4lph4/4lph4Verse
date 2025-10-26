11d36dc6a49d2126b54f24c7f89f10ccc9446a49
cy63r-4lph4 <cy63r.4lph4@gmail.com>
2025-10-26 12:30:02 +0000

    chore: added chain mapping util to enable mutichain relayer support

---
+++ apps/relayer/src/utils/chainImports.ts
@@ -0,0 +1,7 @@
+import { celoSepolia, baseSepolia, liskSepolia } from "viem/chains";
+
+export const supportedChains = {
+  11142220: celoSepolia,        
+  84532: baseSepolia,    
+  4202: liskSepolia     
+};
