11cbee202ba75ffb975e53507276bd52b58d1dcb
cy63r-4lph4 <cy63r.4lph4@gmail.com>
2025-11-08 07:59:53 +0000

    chore: updated the master ignition module to orchestrate the deployment

--- ignition/modules/MasterModule.ts
+++ ignition/modules/MasterModule.ts
@@ -1,13 +1,11 @@
-// ignition/modules/MasterModule.ts
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import CoreModule from "./CoreModule";
 import VerseModule from "./VerseModule";
-// import HireCoreModule from "./HireCoreModule";
 
 export default buildModule("MasterModule", (m) => {
   const core = m.useModule(CoreModule);
   const verse = m.useModule(VerseModule);
-  // const hire = m.useModule(HireCoreModule);
 
-  return { ...core, ...verse,  };
+
+  return { ...core, ...verse };
 });
