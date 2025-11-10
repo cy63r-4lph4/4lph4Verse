fac7fb50fc27fd1790b1709f06e8524c873d903b
cy63r-4lph4 <cy63r.4lph4@gmail.com>
2025-11-10 12:28:51 +0000

    chore: Added a schema resolver typed data util to val

--- packages/sdk/index.ts
+++ packages/sdk/index.ts
@@ -1,16 +1,13 @@
 export * from "./hooks/useBalance";
 export * from "./hooks/useOutsideClick";
 export * from "./hooks/useProfileById";
-export * from "./hooks/useVerseProfileWizard"
+export * from "./hooks/useVerseProfileWizard";
 
-
-
-
-export * from "./utils/builders/permitTypedData"
-export * from "./utils/builders/profileTypedData"
+export * from "./utils/builders/permitTypedData";
+export * from "./utils/builders/profileTypedData";
 export * from "./utils/contract/deployedContracts";
 export * from "./utils/token/tokenUtils";
 export * from "./utils/address/addressUtils";
 export * from "./utils/time/timeUtils";
 export * from "./utils/profile/fetchProfile";
-
+export * from "./config/constants";
