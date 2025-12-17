e3e42a79235b3ab1deb9cc4719faeb53ca360ae0
cy63r-4lph4 <cy63r.4lph4@gmail.com>
2025-12-17 15:15:02 +0000

    chore: Added public folder to @verse/ui to contain all static assets for the verse

---
+++ packages/ui/public/icons/index.ts
@@ -0,0 +1,12 @@
+import Brave from "wallets/brave.svg"
+import Coinbase from "wallets/coinbase.svg"
+import Metamask from "wallets/metamask.svg"
+import Walletconnect from "wallets/walletconnect.svg"
+
+
+
+
+
+
+
+export {Brave,Coinbase,Metamask,Walletconnect}
