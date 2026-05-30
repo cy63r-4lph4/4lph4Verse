07d1ddbda5a1e38d5a1aa4c19129d875d021aff3
cy63r-4lph4 <cy63r.4lph4@gmail.com>
2026-05-30 08:07:21 +0000

    chore: Add site under construction component for pages that are not yet built

---
+++ apps/arena-web/app/course/[id]/practice/page.tsx
@@ -0,0 +1,5 @@
+import UnderConstruction from "@verse/arena-web/components/ui/UnderConstruction";
+
+export default function PracticePage() {
+  return <UnderConstruction sectorName="Practice" />;
+}
